3ff79a9a93ca0a40fc5d2aa9f5298c139b161aed
270648387 <270648387@yoobeestudent.ac.nz>
2025-08-10 11:10:40 +1200

    method

---
+++ method.py
@@ -0,0 +1,22 @@
+class StringManipulator:
+    def __init__(self,text):
+        self.text = text
+
+    def find_char(self,char):
+        pos = self.text.find(char)
+        return f"Position: {pos}" if pos != -1 else "Character not found"
+    
+    def show_leng(self):
+        return(f"Length:{len(self.text)}")
+
+    def upper(self):
+        return(f"Uppercase:{self.text.upper()}")
+    
+name = StringManipulator("example")
+
+result = name.find_char('x')
+leng = name.show_leng()
+upper = name.upper()
+
+print(result,leng,upper)
+
