d489d09514fd8edfb67c9deb13ed00d0d0d2332b
270648387 <270648387@yoobeestudent.ac.nz>
2025-08-16 09:07:35 +1200

    demo

---
+++ Text.py
@@ -0,0 +1,19 @@
+def main():
+    input_path = r"C:\Users\Y\Desktop\PSD\Week3\demo_file.txt"   
+    output_path = r"C:\Users\Y\Desktop\PSD\Week3\demo.txt"       
+    output_path_1 = r"C:\Users\Y\Desktop\PSD\Week3\demo_1.txt"  
+
+    with open(input_path, "r", encoding="utf-8") as infile:
+        content = infile.read()
+
+    with open(output_path, "w", encoding="utf-8") as outfile:
+        outfile.write("Hello World !\n")
+
+    with open(input_path, "a", encoding="utf-8") as outfile1:
+        outfile1.write("Hello World Again!\n")
+    
+    print(f"File '{output_path}' created with first line.")
+    print(f"File '{output_path_1}' appended with second line.")
+
+if __name__ == "__main__":
+    main()
