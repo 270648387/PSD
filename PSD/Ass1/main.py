69dd5e31bf38e431b558611a17b083cba87771a4
270648387 <270648387@yoobeestudent.ac.nz>
2025-09-06 21:29:57 +1200

    Utils

--- PSD/Ass1/main.py
+++ PSD/Ass1/main.py
@@ -1,4 +1,4 @@
-from database import ensure_bootstrap
-first_time = ensure_bootstrap("seed_cars.csv", "car_rental.db")
-print("Database ready (seeded from CSV)" if first_time else "Database ready (existing)")
-# then start your CLI...
+import os
+import database
+from system import CarRentalSystem
+from Utilities import (get_integer_input, get_float_input, get_yes_no_input)
