2159759e96b5bf77f9e10f3cd66a75f0acffa66f
270648387 <270648387@yoobeestudent.ac.nz>
2025-10-12 11:21:08 +1300

    doctest

--- PSD/week11/test.py
+++ PSD/week11/test.py
@@ -1,13 +1,53 @@
 import unittest
+import doctest 
 
 def multiply(x, y):
+    """Returns the mutiply of two numbers.
+    >>> multiply(5,3)
+    15
+    >>> multiply(-8,7)
+    -56
+    """
     return x * y
 
 def add(x, y):
+    """Returns the mutiply of two numbers.
+    >>> add(2,3)
+    5
+    >>> add(-1,1)
+    0
+    """
     return x + y
 
 def minus(x, y):
+    """Returns the minus of two numbers.
+    >>> minus(12, 23)
+    -11
+    >>> minus(-15, 8)
+    -23
+    """
     return x - y
+
+def divide(x, y):
+    """Returns the divident of two numbers.
+    >>> divide(24, 2)
+    12
+    >>> divide(-15, 0)
+    -15
+    """
+    return x / y 
+
+def remainder(x, y):
+    """Returns the remainder of two numbers.
+    >>> remainder(24, 5)
+    4
+    >>> remainder(-15, 2)
+    1
+    """
+    return x % y
+
+
+
 
 class TestMathOperations(unittest.TestCase):
     def test_multiply(self):
@@ -23,7 +63,18 @@ class TestMathOperations(unittest.TestCase):
         self.assertEqual(minus(12, 23), -11)
         self.assertEqual(minus(-15, 8), -23)
 
+    def test_divide(self):
+        self.assertEqual(divide(24,2), 12)
+        self.assertEqual(divide(-15,0), 0)
+
+    def test_remainder(self):
+        self.assertEqual(remainder(24,5), 4)
+        self.assertEqual(remainder(-15,2), 1)
+
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(exit=False)
+    doctest.testmod(verbose=True)
+
+
